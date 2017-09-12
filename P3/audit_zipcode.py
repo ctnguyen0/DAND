77c945ad47073b1d81757391d175f8be284e4f0b
Christopher Nguyen Jr <ctnguyen0@gmail.com>
2017-09-11 20:13:40 -1000

    Update audit_zipcode.py

--- P3/audit_zipcode.py
+++ P3/audit_zipcode.py
@@ -20,3 +20,25 @@ def audit_zip(osmfile):
     return invalid_zipcodes
 
 audited_zipcode_sf = audit_zip(OSMFILE)
+
+
+def update_zip(zipcode):
+    """(str) -> str
+    Return 5 digit zipcodes.
+    """
+    zipChar = re.findall('[a-zA-Z]*', zipcode)
+    if zipChar:
+        zipChar = zipChar[0]
+    zipChar.strip()
+    if zipChar == "CA":
+        updateZip = re.findall(r'\d+', zipcode)
+        if updateZip:
+            return (re.findall(r'\d+', zipcode))[0]
+    else:
+        return (re.findall(r'\d+', zipcode))[0]
+
+def update_zips(datafile):
+    for street_type, ways in sf_zipcode.iteritems():
+        for name in ways:
+            better_name = update_zip(name)
+            print name, "=>", better_name
