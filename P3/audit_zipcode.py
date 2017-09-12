14a9a550ec7426c18208620f93a86929f67b10d4
Christopher Nguyen Jr <ctnguyen0@gmail.com>
2017-09-11 20:13:08 -1000

    Create audit_zipcode.py

---
+++ P3/audit_zipcode.py
@@ -0,0 +1,22 @@
+def audit_zipcode(invalid_zipcodes, zipcode):
+    twoDigits = zipcode[0:2]
+    
+    if twoDigits != 94 or not twoDigits.isdigit():
+        invalid_zipcodes[twoDigits].add(zipcode)
+        
+def is_zipcode(elem):
+    return (elem.attrib['k'] == "addr:postcode")
+
+def audit_zip(osmfile):
+    osm_file = open(osmfile, "r")
+    invalid_zipcodes = defaultdict(set)
+    for event, elem in ET.iterparse(osm_file, events=("start",)):
+
+        if elem.tag == "node" or elem.tag == "way":
+            for tag in elem.iter("tag"):
+                if is_zipcode(tag):
+                    audit_zipcode(invalid_zipcodes,tag.attrib['v'])
+
+    return invalid_zipcodes
+
+audited_zipcode_sf = audit_zip(OSMFILE)
